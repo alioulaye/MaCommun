09db680cd2cb62a040ed47127dccc51e568bcf76
alieu samb <alieusamb@192.168.1.10>
2022-08-20 15:46:35 +0000

    Initial commit

---
+++ src/main/java/com/example/macommun/MaCommunApplication.java
@@ -0,0 +1,13 @@
+package com.example.macommun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaCommunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaCommunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/macommun/MaCommunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macommun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaCommunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
